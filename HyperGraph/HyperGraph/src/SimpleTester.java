b92cbaa92221236ebf6036057a989a88fd81f966
C. Alvin <calvin1@lsu.edu>
2014-11-14 10:38:38 -0700

    Changes to the hypergraph for this specific project. Also, re-engineered the package structure a bit.

---
+++ HyperGraph/HyperGraph/src/SimpleTester.java
@@ -0,0 +1,11 @@
+
+import sequenceDiagramGenerator.hypergraph.*;
+import sequenceDiagramGenerator.SourceCodeType;
+
+public class SimpleTester
+{
+    public static void main(String[] args)
+  	{
+        Hypergraph<SourceCodeType, EdgeAnnotation> graph;
+    }
+}
---
+++ HyperGraph/HyperGraph/src/sequenceDiagramGenerator/hypergraph/HyperEdge.java
@@ -0,0 +1,76 @@
+package sequenceDiagramGenerator.hypergraph;
+
+import java.util.List;
+
+
+public class HyperEdge<A>
+{
+    public List<Integer> sourceNodes;
+    public int targetNode;
+    public A annotation;
+    
+    public HyperEdge(List<Integer> src, int target, A annot)
+    {
+        sourceNodes = src;
+        targetNode = target;
+        annotation = annot;
+
+        if(src.contains(new Integer(target))) {
+            throw new IllegalArgumentException("There exists a direct cycle in a hyperedge" + this);
+        }
+    }
+
+    public boolean DefinesEdge(List<Integer> antecedent, int consequent)
+    {
+        if (targetNode != consequent) return false;
+    	
+    	if (this.sourceNodes.size() != antecedent.size()) return false;
+    	
+        for(Integer ante : antecedent)
+        {
+            if (!sourceNodes.contains(ante)) return false;
+        }
+        
+        return true;
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean equals(Object obj)
+    {
+    	HyperEdge<A> otherEdge = null;
+        try {
+            otherEdge = (HyperEdge<A>)obj;
+        } catch(ClassCastException e)
+        {
+        	return false;
+        }
+
+        if (otherEdge == null) return false;
+
+        //
+        // Verify equality of target node then source nodes.
+        //
+        if (this.targetNode != otherEdge.targetNode) return false;
+        
+        for(Integer src : this.sourceNodes)
+        {
+            if(!otherEdge.sourceNodes.contains(src)) return false;
+        }
+        
+        return true;
+    }
+
+    @Override
+    public String toString()
+    {
+        String retS = " { ";
+        for(Integer node : sourceNodes){
+            retS += node + ",";
+        }
+        if(sourceNodes.size() != 0){retS = retS.substring(0, retS.length() - 1);}
+        retS += " } -> " + targetNode;
+        return retS;
+    }
+}
