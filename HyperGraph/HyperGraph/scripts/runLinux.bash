9070357a1902abfa31de020183123bed799e5ecb
brian <bpete16@lsu.edu>
2015-04-01 11:11:32 -0500

    small script change.

--- HyperGraph/HyperGraph/scripts/runLinux.bash
+++ HyperGraph/HyperGraph/scripts/runLinux.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd ../bin
+rm -rf output
+mkdir output
 java -classpath ".:../lib/sdedit-4.01.jar:../lib/soot-2.5.0.jar" sequenceDiagramGenerator.UI.TestUI -c -jars ../../../TestFiles/TestAnalysis/bin/Test.jar -startmethod Main.main -outfile out.pdf
 java -classpath ".:../lib/sdedit-4.01.jar:../lib/soot-2.5.0.jar" sequenceDiagramGenerator.UI.TestUI -c -jars ../../../TestFiles/TestAnalysis/bin/Test.jar -startmethod Recursion.RecMain.main -outfile out2.pdf
 java -classpath ".:../lib/sdedit-4.01.jar:../lib/soot-2.5.0.jar" sequenceDiagramGenerator.UI.TestUI -c -jars ../../../TestFiles/TestAnalysis/bin/Test.jar -startmethod manybranches.BrMain.main -outfile out3.pdf
