0166010a8c04563edc6fa48a66d3375ea665ee14
Brian Peterson <bpete16@lsu.edu>
2016-03-02 21:56:36 -0600

    adding fix to script per what Raed found.

--- HyperGraph/HyperGraph/scripts/runLinux.bash
+++ HyperGraph/HyperGraph/scripts/runLinux.bash
@@ -5,9 +5,5 @@ rm -rf output
 mkdir output
 rm -rf outputmb
 mkdir outputmb
-%java -classpath ".:../lib/sdedit-4.01.jar:../lib/soot-2.5.0.jar" sequenceDiagramGenerator.UI.TestUI -c -jars ../../../TestFiles/TestAnalysis/bin/Test.jar -startmethod Main.main -outfile out.pdf
-%java -classpath ".:../lib/sdedit-4.01.jar:../lib/soot-2.5.0.jar" sequenceDiagramGenerator.UI.TestUI -c -jars ../../../TestFiles/TestAnalysis/bin/Test.jar -startmethod Recursion.RecMain.main -outfile out2.pdf
-%java -classpath ".:../lib/sdedit-4.01.jar:../lib/soot-2.5.0.jar" sequenceDiagramGenerator.UI.TestUI -c -jars ../../../TestFiles/TestAnalysis/bin/Test.jar -startmethod manybranches.BrMain.main -outfile out3.pdf
-%java -classpath ".:../lib/sdedit-4.01.jar:../lib/soot-2.5.0.jar" sequenceDiagramGenerator.UI.TestUI -a -jars ../../../TestFiles/TestAnalysis/bin/Test.jar -startmethod manybranches.BrMain.main -outdir output
-java -classpath ".:../lib/sdedit-4.01.jar:../lib/soot-2.5.0.jar" sequenceDiagramGenerator.UI.TestUI -a -jars ../../../TestFiles/TestAnalysis/bin/Test.jar -startmethod branchdepth.Br2Main.main -outdir outputmb
+java -classpath ".:../lib/sdedit-4.01.jar:../lib/soot-2.5.0.jar:../lib/json-simple-1.1.1.jar" sequenceDiagramGenerator.UI.TestUI -c -jars ../../../TestFiles/TestAnalysis/bin/Test.jar -startmethod "Main.main(java.lang.String[])" -outfile out.pdf
 cd ../scripts
